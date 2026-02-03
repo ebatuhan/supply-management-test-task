7a3275b658fa5075ecc51294d0f8d21d08b0867b
ebatuhan <batuhane2401@gmail.com>
2026-02-03 18:27:00 +0300

    init

---
+++ src/main/java/com/batu/supply_management_test_task/SupplyManagementTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.batu.supply_management_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyManagementTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyManagementTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batu/supply_management_test_task/SupplyManagementTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batu.supply_management_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyManagementTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
